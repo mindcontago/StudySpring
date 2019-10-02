41333f9cfe8676560378ffad6f6319753d970c9c
mindcontago <55882050+mindcontago@users.noreply.github.com>
2019-10-02 17:37:50 +0300

    Main class added (OurApplication)
    to start on something)

---
+++ StudySpring/src/main/java/demo/OurApplication.java
@@ -0,0 +1,9 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class OurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OurApplication.class, args);
+    }
+}
