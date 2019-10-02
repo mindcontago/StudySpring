af1c803a0ec259a992eb94e307b4429e42b81511
mindcontago <55882050+mindcontago@users.noreply.github.com>
2019-10-02 16:52:01 +0300

    Add files via upload
    Data base added

---
+++ StudySpring/src/main/java/demo/OurDataBaseDto.java
@@ -0,0 +1,29 @@
+package demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class OurDataBaseDto {
+
+        public String name;
+        public String number;
+
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public String getNumber() {
+//            return number;
+//        }
+//
+//        public void setNumber(String number) {
+//            this.number = number;
+//        }
+}
