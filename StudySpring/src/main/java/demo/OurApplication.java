7ad3b3c1fc122d4ed2331815bd6ecb5e04308b44
mindcontago <55882050+mindcontago@users.noreply.github.com>
2019-10-03 10:30:25 +0300

    appplication properties added
    (but error in browser)

--- StudySpring/src/main/java/demo/OurApplication.java
+++ StudySpring/src/main/java/demo/OurApplication.java
@@ -1,9 +1,13 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class OurApplication {
     public static void main(String[] args) {
         SpringApplication.run(OurApplication.class, args);
     }
 }
+
+
